aca79ee5ccd77a3e409cec997b4589ccdc452a55
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2020-12-11 11:22:44 -0800

    add space after any colons

--- CH1/Exercises/ex_03_02.py
+++ CH1/Exercises/ex_03_02.py
@@ -1,23 +1,25 @@
 
-try:
+inperr = "Error, please enter numberic input"
+
+try :
     hours = float(input("Enter hours: "))
-except:
-    print("Error, please enter numberic input")
+except :
+    print(inperr)
     quit()    
 
-try:
+try :
     rate = float(input("Enter Rate: "))
-except:
-    print("Error, please enter numberic input")
+except :
+    print(inperr)
     quit()  
 
 maxhours = 40
 # Calculate pay, no overtime
-if hours <= maxhours:
+if hours <= maxhours :
     pay = hours * rate
 
 # Calculate total pay including overtime
-else:
+else :
     ot_hours = hours - maxhours
     ot_rate = rate * 1.5
     pay = (maxhours * rate) + (ot_hours * ot_rate)
