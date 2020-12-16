ccdc5a3b549d60f7c2a09e31e6d10523d5dabac7
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2020-12-16 14:23:45 -0800

    more changes

--- CH1/word_count.py
+++ CH1/word_count.py
@@ -1,5 +1,3 @@
-
-
 class WordCount():
     def openfile(self, fname):
         """
@@ -19,8 +17,12 @@ class WordCount():
         return line
 
     def get_word_with_largest_count(self, line):
-        
+        new_line = line.splitlines()
+        word_list = new_line.split(sep=' ')
+        dic = dict()
+        for word in word_list:
+            dic[word] = dic[word] + 1
         return {'to': 3}
-    
+
     def check_for_newer_largest_count(self, top_word):
         return {"to": 4}
