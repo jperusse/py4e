b865b4e0d1aec87f066069d8789d68c0c69efe6a
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2020-12-18 11:56:21 -0800

    save search7

--- Exercises/ex_07_01/ex_07_01.py
+++ Exercises/ex_07_01/ex_07_01.py
@@ -1,2 +1,18 @@
-fi = 'C:\\Users\\xtech\\Documents\\GitHub\\py4e\\code3\\mbox.txt'
-print(fi)
+samples = 'C:\\Users\\xtech\\Documents\\GitHub\\py4e\\code3\\'
+# file = 'mbox.txt'
+file = 'mbox-short.txt'
+
+full_path = samples + file
+
+try:
+    fh = open(full_path, 'r')
+except:
+    print('File not found: ' + full_path)
+    quit()
+
+print('File opened', full_path)
+count = 0
+for line in fh :
+    if line.find('@uct.ac.za') == -1 : continue
+    line = line.rstrip()
+    print("'" + line + "'")
---
+++ Exercises/ex_07_01/search7.py
@@ -0,0 +1,13 @@
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File not found:', fname)
+    quit()
+    
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print('There were', count, 'subject lines in', fname)
+
