87233163417b1e89d2d76535f92a7be1004d14bc
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2020-12-27 11:36:32 -0800

    Exercise 9.4

---
+++ Exercises/ex_09_04/ex_09_04.py
@@ -0,0 +1,30 @@
+file = 'mbox.txt'
+# file = 'mbox-short.txt'
+
+try:
+    fh = open(file, 'r')
+except:
+    print('File not found: ' + file)
+    quit()
+
+print('File opened', file)
+
+addesses = dict()
+for line in fh:
+    words = line.split()
+
+    if len(words) < 2 or words[0] != 'From': continue
+
+    addess = words[1]
+    addesses[addess] = addesses.get(addess, 0) + 1
+
+big_key = None
+
+for key,value in addesses.items():
+    if big_key is None or value > big_value:
+        big_key = key
+        big_value = value
+print(big_key, big_value)
+# print('Count of addresses found:', len(addesses))
+
+# print(addesses)
