e8397f287b24ebf7cc798108b15e09c8466c6b50
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2021-01-08 17:57:47 -0800

    regexes done

--- tests/Exercises/Class03_Access_Web_Data/test_utils.py
+++ tests/Exercises/Class03_Access_Web_Data/test_utils.py
@@ -50,19 +50,19 @@ class TestExerciseUtils():
     def test_run_findall_is_found(self):
         assert self.exu.run_findall("mbox-short5.txt", "\\S+@\\S+", False) == 5
         # Sample each of the special regex characters
-        assert self.exu.run_findall("mbox-short5.txt", "^From",     True) == 1  # Matches the beginning of a line
-        assert self.exu.run_findall("mbox-short5.txt", ";$",        True) == 3  # Matches the end of the line
-        assert self.exu.run_findall("mbox-short5.txt", "F...",      True) == 1  # Matches any character
-        assert self.exu.run_findall("mbox-short5.txt", "\\s+for",   True) == 2  # Matches whitespace
-        # assert self.exu.run_findall("mbox-short5.txt", "\\S+-Path", True) == 5  # Matches non-whitespace character
-        # assert self.exu.run_findall("mbox-short5.txt", ".*for",     True) == 5  # Repeats a character zero or more times
-        # assert self.exu.run_findall("mbox-short5.txt", "\\s*?for",  True) == 5  # zero or more characters (not-greedy)
-        # assert self.exu.run_findall("mbox-short5.txt", "\\s+for",   True) == 5  # Repeats a character one or more times
-        # assert self.exu.run_findall("mbox-short5.txt", "\\s+?for",  True) == 5  # Repeats a character one or more times (non-greedy)
-        # assert self.exu.run_findall("mbox-short5.txt", "[aeiou]",   True) == 5  # Matches a single character in the listed set
-        # assert self.exu.run_findall("mbox-short5.txt", "^XYZ",      True) == 5  # Matches a single character not in the listed set
-        # assert self.exu.run_findall("mbox-short5.txt", "[a-z0-9]",  True) == 5  # The set of characters can include a range
-        # assert self.exu.run_findall("mbox-short5.txt", "(from.+@.+",True) == 5  # Indicates where string extraction is to start and end
+        assert self.exu.run_findall("mbox-short5.txt", "^From",         True) == 1  # Matches the beginning of a line
+        assert self.exu.run_findall("mbox-short5.txt", ";$",            True) == 3  # Matches the end of the line
+        assert self.exu.run_findall("mbox-short5.txt", "F...",          True) == 1  # Matches any character
+        assert self.exu.run_findall("mbox-short5.txt", "\\s+for",       True) == 2  # Matches whitespace
+        assert self.exu.run_findall("mbox-short5.txt", "\\S+-Path",     True) == 1  # Matches non-whitespace character
+        assert self.exu.run_findall("mbox-short5.txt", "\\s*[0-9]",     True) == 3  # Repeats a character zero or more times
+        assert self.exu.run_findall("mbox-short5.txt", "\\s*?[0-9]",    True) == 3  # Repeats a character zero or more times (not-greedy)
+        assert self.exu.run_findall("mbox-short5.txt", "\\s+[0-9]+",    True) == 1  # Repeats a character one or more times
+        assert self.exu.run_findall("mbox-short5.txt", "\\s+?[0-9]+?",  True) == 1  # Repeats a character one or more times (not-greedy)
+        assert self.exu.run_findall("mbox-short5.txt", "[aeiou]",       True) == 8  # Matches a single character in the listed set
+        assert self.exu.run_findall("mbox-short5.txt", "[^aeiou;]",     True) == 10  # Matches a single character not in the listed set
+        assert self.exu.run_findall("mbox-short5.txt", "[a-z]",         True) == 8  # The set of characters can include a range
+        assert self.exu.run_findall("mbox-short5.txt", "\\S+@(\\S+)",   True) == 5  # Indicates where string extraction is to start and end
 
     def test_run_findall_is_not_found(self):
         assert self.exu.run_findall("mbox-short5.txt", "JIMBO-missing", False) == 0
