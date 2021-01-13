e4bd9f5f0a759fa846df0d422c3edcc58519d018
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2021-01-12 16:20:31 -0800

    complete urljpeg

--- Exercises/Class03_Access_Web_Data/utils.py
+++ Exercises/Class03_Access_Web_Data/utils.py
@@ -7,6 +7,14 @@ class ExerciseUtils():
     Test methods common to PY4E exercises
     """
 
+    def init_socket(self, url_page):
+        url_prefix = "http://"
+        url_base = "data.pr4e.org"
+        url = url_prefix + url_base + "/" + url_page
+        mysock = self.open_socket("data.pr4e.org", 80)  # normal socket
+        assert not mysock._closed
+        return mysock, url
+
     def open_socket(self, host, port):
         """
         open a socket for host on port
@@ -33,14 +41,14 @@ class ExerciseUtils():
 
     def get_page(self, mysock, url):
         """
-        get data from a socket
+        get  a text document from a socket
         """
         page = list()
 
         cmd = 'GET ' + url + ' HTTP/1.0\r\n\r\n'
         cmd = cmd.encode()
         try:
-            mysock.send(cmd)
+            mysock.sendall(cmd)
         except:
             print('SEND failed for ', url)
             return page
@@ -53,6 +61,49 @@ class ExerciseUtils():
             print(page_data, end='')
 
         return page
+
+    def get_jpeg(self, mysock, url):
+        """
+        get a jpeg document from a socket
+        """
+        count = 0
+        picture = b""
+
+        cmd = 'GET ' + url + ' HTTP/1.0\r\n\r\n'
+        cmd = cmd.encode()
+        try:
+            mysock.send(cmd)
+        except:
+            print('SEND failed for ', url)
+            return picture
+
+        while True:
+            data = mysock.recv(5120)
+            if len(data) < 1:
+                break
+
+            # time.sleep(0.25)
+            count = count + len(data)
+            print(len(data), count)
+            picture = picture + data
+
+        return picture
+
+    def save_picture(self, picture, file):
+        """
+        Strip headers from picture and save to file
+        """
+        # Look for the end of the header (2 CRLF)
+        pos = picture.find(b"\r\n\r\n")
+        print('Header length', pos)
+        print(picture[:pos].decode())
+
+        # Skip past the header and save the picture data
+        picture = picture[pos+4:]
+        fhand = open(file, "wb")
+        fhand.write(picture)
+        fhand.close()
+        return len(picture)
 
     def openfile(self, fname, mode):
         """
@@ -180,7 +231,8 @@ assert count == 336
 
 print("re07 - Search for lines that have an at sign between characters")
 exu = ExerciseUtils()
-count = exu.run_findall('mbox.txt', '[a-zA-Z0-9][a-zA-Z0-9.]*@[a-zA-Z][a-zA-Z.]*', False)
+count = exu.run_findall(
+    'mbox.txt', '[a-zA-Z0-9][a-zA-Z0-9.]*@[a-zA-Z][a-zA-Z.]*', False)
 assert count == 22009
 
 print("re10 - Search for lines that start with 'X' followed by any non whitespace characters and ':' followed by a space and any number. The number can include a decimal.")
@@ -198,17 +250,25 @@ exu = ExerciseUtils()
 count = exu.run_findall('mbox-short.txt', '^From .* ([0-9][0-9]):', False)
 assert count == 27
 
-print("socket1 - World's simplest web browswer")
+print("socket1 - World's simplest web browser")
 exu = ExerciseUtils()
-url_prefix = "http://"
-url_base = "data.pr4e.org"
-url_page = "romeo.txt"
-url = url_prefix + url_base + "/" + url_page
-mysock = exu.open_socket("data.pr4e.org", 80) # normal socket
-assert not mysock._closed
+mysock, url = exu.init_socket("romeo.txt")
 
 page = exu.get_page(mysock, url)
 assert len(page) == 2
 
-mysock = exu.close_socket(mysock) # normal socket
+mysock = exu.close_socket(mysock)  # normal socket
 assert mysock._closed
+
+print("urljpeg - get a jpeg document")
+exu = ExerciseUtils()
+mysock, url = exu.init_socket("cover3.jpg")
+
+pic = exu.get_jpeg(mysock, url)
+assert len(pic) == 230608
+
+mysock = exu.close_socket(mysock)  # normal socket
+assert mysock._closed
+
+rc = exu.save_picture(pic, "stuff.jpg")
+assert rc > 0
--- tests/Exercises/Class03_Access_Web_Data/test_utils.py
+++ tests/Exercises/Class03_Access_Web_Data/test_utils.py
@@ -18,16 +18,16 @@ class TestExerciseUtils():
             line = ''
         return line
 
-    @pytest.fixture(autouse=True)
+    @pytest.fixture(autouse=False)
     def ReadFirstLine(self):
         self.fh = self.exu.openfile(self.fname, 'r')
         self.next_line = self.read_next_line(self.fh)
 
-    def test_openfile_raises_file_not_found(self):
+    def test_openfile_raises_file_not_found(self, ReadFirstLine):
         fh = self.exu.openfile("missing", 'r')
         assert fh == ""
 
-    def test_read_next_line_from_file(self):
+    def test_read_next_line_from_file(self, ReadFirstLine):
         assert self.next_line.strip() == self.firstline
 
     def test_run_search1_is_empty(self):
@@ -41,47 +41,133 @@ class TestExerciseUtils():
         assert self.exu.run_search1('mbox-short.txt', '^From:.+@', False) == 27
 
     def test_run_search1_is_not_found(self):
-        assert self.exu.run_search1("mbox-short.txt", "JIMBO-missing", False) == 0
-
+        assert self.exu.run_search1(
+            "mbox-short.txt", "JIMBO-missing", False) == 0
 
     def test_run_findall_is_empty(self):
         assert self.exu.run_findall("mbox-short5.txt", "", False) == 0
 
     def test_run_findall_is_found(self):
         # Sample each of the special regex characters
-        assert self.exu.run_findall("mbox-short5.txt", "^From",         True) == 1  # Matches the beginning of a line
-        assert self.exu.run_findall("mbox-short5.txt", ";$",            True) == 3  # Matches the end of the line
-        assert self.exu.run_findall("mbox-short5.txt", "F...",          True) == 1  # Matches any character
-        assert self.exu.run_findall("mbox-short5.txt", "\\s+for",       True) == 2  # Matches whitespace
-        assert self.exu.run_findall("mbox-short5.txt", "\\S+-Path",     True) == 1  # Matches non-whitespace character
-        assert self.exu.run_findall("mbox-short5.txt", "\\s*[0-9]",     True) == 9  # Repeats a character zero or more times
-        assert self.exu.run_findall("mbox-short5.txt", "\\s*?[0-9]",    True) == 9  # Repeats a character zero or more times (not-greedy)
-        assert self.exu.run_findall("mbox-short5.txt", "\\s+[0-9]+",    True) == 7  # Repeats a character one or more times
-        assert self.exu.run_findall("mbox-short5.txt", "\\s+?[0-9]+?",  True) == 7  # Repeats a character one or more times (not-greedy)
-        assert self.exu.run_findall("mbox-short5.txt", "[aeiou]",       True) == 14 # Matches a single character in the listed set
-        assert self.exu.run_findall("mbox-short5.txt", "[^aeiou;]",     True) == 16 # Matches a single character not in the listed set
-        assert self.exu.run_findall("mbox-short5.txt", "[a-z]",         True) == 14  # The set of characters can include a range
-        assert self.exu.run_findall("mbox-short5.txt", "\\S+@(\\S+)",   True) == 5  # Indicates where string extraction is to start and end
+        # Matches the beginning of a line
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "^From",         True) == 1
+        # Matches the end of the line
+        assert self.exu.run_findall(
+            "mbox-short5.txt", ";$",            True) == 3
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "F...",          True) == 1  # Matches any character
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "\\s+for",       True) == 2  # Matches whitespace
+        # Matches non-whitespace character
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "\\S+-Path",     True) == 1
+        # Repeats a character zero or more times
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "\\s*[0-9]",     True) == 9
+        # Repeats a character zero or more times (not-greedy)
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "\\s*?[0-9]",    True) == 9
+        # Repeats a character one or more times
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "\\s+[0-9]+",    True) == 7
+        # Repeats a character one or more times (not-greedy)
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "\\s+?[0-9]+?",  True) == 7
+        # Matches a single character in the listed set
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "[aeiou]",       True) == 14
+        # Matches a single character not in the listed set
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "[^aeiou;]",     True) == 16
+        # The set of characters can include a range
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "[a-z]",         True) == 14
+        # Indicates where string extraction is to start and end
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "\\S+@(\\S+)",   True) == 5
 
     def test_run_findall_is_not_found(self):
-        assert self.exu.run_findall("mbox-short5.txt", "JIMBO-missing", False) == 0
+        assert self.exu.run_findall(
+            "mbox-short5.txt", "JIMBO-missing", False) == 0
 
     def test_run_findall_avg_is_empty(self):
-        count, avg =  self.exu.run_findall_avg("mbox-short.txt", "", True)
+        count, avg = self.exu.run_findall_avg("mbox-short.txt", "", True)
         assert count == 0
         assert avg == 0
 
     def test_run_findall_avg_is_found(self):
         # Sample each of the special regex characters
-        count, avg =  self.exu.run_findall_avg("mbox-short6.txt", "^From", True)  # skipped because string is not an number
+        # skipped because string is not an number
+        count, avg = self.exu.run_findall_avg("mbox-short6.txt", "^From", True)
         assert count == 0
         assert avg == 0
 
-        count, avg =  self.exu.run_findall_avg("mbox-short6.txt", "Counting: ([0-9])", True)
+        count, avg = self.exu.run_findall_avg(
+            "mbox-short6.txt", "Counting: ([0-9])", True)
         assert count == 12
         assert avg == 2
 
     def test_run_findall_avg_is_not_found(self):
-        count, avg =  self.exu.run_findall_avg("mbox-short6.txt", "JIMBO-missing", False)
+        count, avg = self.exu.run_findall_avg(
+            "mbox-short6.txt", "JIMBO-missing", False)
         assert count == 0
         assert avg == 0
+
+    def test_open_socket_bad_host(self):
+        mysock = self.exu.open_socket("", 80)
+        assert mysock == None
+
+    def test_open_socket_bad_port(self):
+        mysock = self.exu.open_socket("data.pr4e.org", 0)
+        assert mysock == None
+
+    def test_open_socket_for_good_host(self):
+        mysock = self.exu.open_socket("data.pr4e.org", 80)  # normal socket
+        assert mysock._closed == False
+
+    def test_close_socket_for_good_host(self):
+        mysock = self.exu.open_socket("data.pr4e.org", 80)  # normal socket
+        assert mysock._closed == False
+
+        mysock = self.exu.close_socket(mysock)  # normal socket
+        assert mysock._closed
+
+    def test_get_page(self):
+        mysock, url = self.exu.init_socket("romeo.txt")
+
+        page = self.exu.get_page(mysock, url)
+        assert len(page) == 2
+
+        mysock = self.exu.close_socket(mysock)  # normal socket
+        assert mysock._closed
+
+        page = self.exu.get_page(mysock, url)
+        assert len(page) == 0
+
+    def test_get_picture(self):
+        mysock, url = self.exu.init_socket("cover3.jpg")
+
+        picture = self.exu.get_jpeg(mysock, url)
+        assert len(picture) == 230608
+
+        mysock = self.exu.close_socket(mysock)  # normal socket
+        assert mysock._closed
+
+        picture = self.exu.get_jpeg(mysock, url)
+        assert len(picture) == 0
+
+    def test_get_jpeg(self):
+        mysock, url = self.exu.init_socket("cover3.jpg")
+
+        picture = self.exu.get_jpeg(mysock, url)
+        assert len(picture) == 230608
+
+        mysock = self.exu.close_socket(mysock)  # normal socket
+        assert mysock._closed
+        
+        rc = self.exu.save_picture(picture, "stuff.jpg")
+        assert rc > 0
+
+        picture = self.exu.get_jpeg(mysock, url)
+        assert len(picture) == 0
