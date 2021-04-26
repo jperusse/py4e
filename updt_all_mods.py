048914b0beea78bd929528390e579f2d8ba7f8ac
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2021-04-26 12:09:39 -0700

    Update modules

--- updt_all_mods.py
+++ updt_all_mods.py
@@ -4,5 +4,11 @@ import subprocess as sbp
 import pip
 pkgs = eval(str(sbp.run("pip3 list -o --format=json", shell=True,
                         stdout=sbp.PIPE).stdout, encoding='utf-8'))
+if len(pkgs) == 0:
+    print('Nothing to update')
+    quit()
+
+print(str(len(pkgs)) + ' modules to update found. Beginning update proess...')
 for pkg in pkgs:
+    print(pkg)
     sbp.run("pip3 install --upgrade " + pkg['name'], shell=True)
